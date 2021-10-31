4bb8a179922953f105452700dc0540c35a1408cb
lehdemetrio <86389762+lehdemetrio@users.noreply.github.com>
2021-10-31 14:37:12 -0300

    Método adição

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -8,12 +8,23 @@ namespace Calculadora
         {
             int n1 = null
             int n2 = null
-                
+            var operacao = null
+            int resultado = null
                 
             Console.WriteLine("Insira o primeiro número");
             Console.Readline(n1);
-            Console.Writeline("insira o segundo numero")
+            Console.Writeline("insira o segundo numero");
             Console.Readline(n2);
+            Console.Writeline("selecione a operação desejada");
+            Console.Readline(operacao);
+            
+            if(operacao = +)
+            {
+            resultado = n1+n2
+            }
+            
+            else
+            
         }
     }
 }
